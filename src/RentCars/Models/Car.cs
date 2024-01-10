30cdbc9829b1aded119116902ae86b1ef9387552
jadsoncerqueira <jadsoncerqueira97@gmail.com>
2024-01-10 17:52:09 -0300

    Revisando requisitos

--- src/RentCars/Models/Car.cs
+++ src/RentCars/Models/Car.cs
@@ -4,9 +4,9 @@ namespace RentCars.Models;
 //6 - Faça a classe `Car` herdar de `Vehicle`
 public class Car: Vehicle
 {
-  public int Seats;
-  public int Doors;
-  public TractionType Traction;
-  public BrakeType FrontBrake;
-  public BrakeType RearBrake;
+  public int Seats { get; set; }
+  public int Doors { get; set; }
+  public TractionType Traction { get; set; }
+  public BrakeType FrontBrake { get; set; }
+  public BrakeType RearBrake { get; set; }
 }
