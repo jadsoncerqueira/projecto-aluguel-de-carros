fda6e6dbc5690340e0372a7c709af7c7faa8cb7d
jadsoncerqueira <jadsoncerqueira97@gmail.com>
2024-01-10 17:05:38 -0300

    Adicionando propriedades a classe Car e aplicando herança coma classse vehicle

--- src/RentCars/Models/Car.cs
+++ src/RentCars/Models/Car.cs
@@ -2,6 +2,11 @@ using RentCars.Types;
 namespace RentCars.Models;
 
 //6 - Faça a classe `Car` herdar de `Vehicle`
-public class Car
+public class Car: Vehicle
 {
+  public int Seats;
+  public int Doors;
+  public TractionType Traction;
+  public BrakeType FrontBrake;
+  public BrakeType RearBrake;
 }
