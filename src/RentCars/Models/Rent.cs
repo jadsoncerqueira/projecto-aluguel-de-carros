38a9e8dc2238939511aa0ffb9dc6934b850bafde
jadsoncerqueira <jadsoncerqueira97@gmail.com>
2024-01-10 18:02:00 -0300

    Revisando requisitos

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -15,7 +15,7 @@ public class Rent
     {
        double preco = vehicle.PricePerDay * daysRented;
        if(person is LegalPerson) {
-        preco -= preco / 100 * 10 ;
+        preco -= preco * 0.10 ;
        } ;
        Price = preco;
        Status = RentStatus.Confirmed;
